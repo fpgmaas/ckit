799403e0babd075a85ef8916a8328e0d7e537ec6
Florian Maas <fpgmaas@gmail.com>
2023-03-25 08:49:49 +0100

    Added method to download global config files

--- ckit/config/config_files_initiatior.py
+++ ckit/config/config_files_initiatior.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import urllib.request
 from dataclasses import dataclass
 from pathlib import Path
 
 import click
+import requests
 
 from ckit.config.common import get_global_commands_dir
 
@@ -60,13 +62,26 @@ example-groups:
 
 @dataclass
 class ConfigFilesInitiator:
+    """
+    Args:
+      download_global_defaults (bool): If False, simply prompt to create ckit.yaml in the global config directory. If True,
+      prompt to download files from the ckit-files repository.
+      skip_global: Skip initializing the global configuration directory.
+      skip_local: Skip initializing the local configuration file.
+    """
+
+    download_global_defaults: bool = False
+    skip_global: bool = False
+    skip_local: bool = False
+
     def init(self):
-        """
-        Check for the existence of local- and global ckit.yaml files, and if they do not exist,
-        ask the user if they should be created.
-        """
-        self._init_global()
-        self._init_local()
+        if not self.skip_global:
+            if self.download_global_defaults:
+                self._init_global_by_downloading_config_files()
+            else:
+                self._init_global()
+        if not self.skip_local:
+            self._init_local()
 
     def _init_local(self):
         """
@@ -85,11 +100,31 @@ class ConfigFilesInitiator:
         Check if there is a global ckit.yaml file. If it does not exist, ask the user if it should be created, and
         if so, create it.
         """
-        global_commands_file = get_global_commands_dir() / "ckit.yaml"
+        global_commands_dir = get_global_commands_dir()
+        global_commands_file = global_commands_dir / "ckit.yaml"
         if global_commands_file.exists():
             click.echo(f"{global_commands_file} already exists.")
         else:
-            if click.confirm(f"Create a global ckit.yaml file in {global_commands_file.parent}?", default=True):
-                global_commands_file.parent.mkdir(parents=True, exist_ok=True)
+            if click.confirm(f"Create a global ckit.yaml file in {global_commands_dir}?", default=True):
+                global_commands_dir.mkdir(parents=True, exist_ok=True)
                 with open(global_commands_file, "w") as f:
                     f.write(COMMANDS_YAML_DEFAULT)
+
+    def _init_global_by_downloading_config_files(self):
+        """
+        Download config files from the `ckit-files` repository.
+        """
+        global_commands_dir = get_global_commands_dir()
+        config_files = self._list_config_files_from_github()
+        if click.confirm(
+            f"Download the files {[file['name'] for file in config_files]} and place them in {global_commands_dir}?",
+            default=True,
+        ):
+            global_commands_dir.mkdir(parents=True, exist_ok=True)
+            for file in config_files:
+                urllib.request.urlretrieve(file["url"], global_commands_dir / file["name"])
+
+    def _list_config_files_from_github(self):
+        url = "https://api.github.com/repos/fpgmaas/ckit-files/contents/config-files"
+        response_json = requests.get(url).json()
+        return [{"name": x["name"], "url": x["download_url"]} for x in response_json]
